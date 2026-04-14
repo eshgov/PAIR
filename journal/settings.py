b6ecc0a0452771a3f16a35fde30ddafd00902e4e
codejvn <codejvn@gmail.com>
2026-04-13 23:38:55 -0400

    updating settings and version

--- journal/settings.py
+++ journal/settings.py
@@ -21,10 +21,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["pair-dj8z.onrender.com", "localhost", "127.0.0.1"]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
